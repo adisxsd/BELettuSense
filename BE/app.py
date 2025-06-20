6917fac95379aba67c104f9b57334ae0f1b62b9c
Nurul Hadits <117652857+adisxsd@users.noreply.github.com>
2025-06-20 10:29:50 +0700

    Update app.py

--- BE/app.py
+++ BE/app.py
@@ -108,6 +108,11 @@ def riwayat(user_id):
         })
 
     return jsonify({'riwayat': riwayat_list})
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 # === Start server ===
 if __name__ == '__main__':
